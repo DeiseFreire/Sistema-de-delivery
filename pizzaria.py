bb535571b43fb6c97c7acef57be7aa8315ff0c01
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2020-11-09 15:33:48 -0300

    Create pizzaria.py

---
+++ pizzaria.py
@@ -0,0 +1,23 @@
+from tkinter import*
+import time
+import random
+import tkinter import messagebox 
+class Pizza: 
+def main(sf):
+sf.scr.destroy 
+class Pizza:
+def main(sf):
+try:
+sf.scr.destroy()
+sf.scr=Tk()
+except:
+try:
+sf.scr=Tk()
+except:
+pass 
+sf.scr.geometry("1366x768")
+sf.scr.title("Pizzaria Sangiorgio: ")
+sf.scr.iconbitmap('p.ico')
+sf.scr.mainloop()
+x = Pizza()
+x.main()
