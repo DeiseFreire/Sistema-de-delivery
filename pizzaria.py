2ac89afb8289e1a8675098e86b932bdb6f108622
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2020-11-24 20:00:09 -0300

    Update pizzaria.py

--- pizzaria.py
+++ pizzaria.py
@@ -23,15 +23,19 @@ sf.logo = PhotoImage(file="logo1.png')
 sf.l = Label(sf.mainf1,image=sf.logo)
 sf.l.place(x=0,y=0)
 sf.mainf1.pack(fill=BOTH,expand=1)
-sf.mainf2 = frame(sf.scr, height=618,width=1366)
-sf.c = Canvas(sf.mainf2,image=sf.logo)
+
+sf.mainf2 = Frame(sf.scr, height=618,widht=1366)
+sf.c = Canvas(sf.mainf2, height=618,width=1366)
+sf.back = PhotoImage(file="pizzamain.png")
+
 sf.c.pack()
 sf.mainf1.pack(fill=BOTH,expand=1)
+
 sf.scr.mainloop()
 x=Pizza()
 x.main()
-sf.mainf1 = 
-Label(sf.mainf1,image=sf.logo)
+
+sf.mainf1 = Label(sf.mainf1,image=sf.logo)
 
 
 
